8f91f6a0ed59f685dce3b35fb72ab3ad4921c74c
jiyanpatil07 <jiyanpatil0707@gmail.com>
2022-02-04 23:28:03 +0530

    End of Node basic

--- Event-Emitter.js
+++ Event-Emitter.js
@@ -1,1 +1,10 @@
-console.log("jiyan");
+const EventEmitter=require('events')
+const CustomEvent=new EventEmitter()
+CustomEvent.on('response',(name,id)=>{
+    console.log(`data received, ${name} , ${id}`);
+})
+CustomEvent.on('response',()=>{
+    console.log("data received");
+})
+CustomEvent.emit('response','jihn',90)
+CustomEvent.emit('response')
