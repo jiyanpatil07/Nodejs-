da5dd8bb94d92ede506e10a0dbfc102d4e31ebc3
jiyanpatil07 <jiyanpatil0707@gmail.com>
2022-02-04 22:30:58 +0530

    Events added

---
+++ asyncFunc.js
@@ -0,0 +1,9 @@
+// console.log("first");
+// setTimeout(() => {
+//    console.log("second"); 
+// },0 );
+// console.log("third");
+setInterval(() => {
+    console.log("hello world");
+}, 1000);
+console.log("task completed");
---
+++ server2.js
@@ -0,0 +1,19 @@
+const http=require('http')
+console.log("jiyan");
+const server=http.createServer((req,res)=>{
+    if (req.url==='/') {
+        
+        res.end("HOme page")
+    } 
+    if(req.url==='/about') {
+        // for (let i = 0; i < 1000; i++) {
+        //     for (let j = 0; j < 1000; j++) {
+        //         console.log(`${i} ${j}`);
+        //     }
+        // }
+        res.end("about page")
+    }
+})
+server.listen(5000,()=>{
+    console.log('Server is liseting on 5000');
+})
